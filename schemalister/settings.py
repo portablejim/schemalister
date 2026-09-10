4e4511e9ec6fd21d90ad4b426fb0764689c0e74f
James McKay <portablejim@jamesmckay.id.au>
2026-09-10 23:45:51 +1000

    Remove separate unused JWT secret environment variable

--- schemalister/settings.py
+++ schemalister/settings.py
@@ -34,7 +34,6 @@ IS_LOCAL = ENVIRONMENT == 'dev'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("DJANGO_SECRET_KEY")
-JWT_SECRET_KEY = env("JWT_SECRET_KEY")
 SECRETS_ENCRYPTION_KEY = env("SECRETS_ENCRYPTION_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
