1831b281e43f9c92cd9d6c1de57add879ece5282
James McKay <portablejim@jamesmckay.id.au>
2026-09-10 11:07:02 +1000

    Add method to suggest missing keys if not in environment varibles

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+from dotenv import load_dotenv
+import logging
 import os
 import sys
 
-from dotenv import load_dotenv
+logger = logging.getLogger(__name__) 
 
 
 def main():
@@ -13,12 +15,24 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schemalister.settings')
     try:
         from django.core.management import execute_from_command_line
+        from django.core.management.utils import get_random_secret_key
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # If the environment is not production, do extra checks
+    if 'ENVIRONMENT' not in os.environ or os.environ["ENVIRONMENT"] != 'production':
+        missing_django_secret_key = 'DJANGO_SECRET_KEY' not in os.environ or os.environ['DJANGO_SECRET_KEY'] is None or len(os.environ['DJANGO_SECRET_KEY']) < 1
+        if missing_django_secret_key:
+            logging.critical('There seems to be environment variables missing. It is dangerous to go alone! Take this!')
+            if missing_django_secret_key:
+                logging.critical('DJANGO_SECRET_KEY missing | Example: ' + get_random_secret_key())
+            exit(1)
+
+
     execute_from_command_line(sys.argv)
 
 
