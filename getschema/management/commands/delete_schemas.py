14258db1383dedb9b2643fe4feb781229152cd6c
James McKay <portablejim@jamesmckay.id.au>
2026-09-13 14:08:54 +1000

    Have delete_schemas command also delete abandoned schemas

--- getschema/management/commands/delete_schemas.py
+++ getschema/management/commands/delete_schemas.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand
+from django.utils import timezone
+
 from getschema.models import Schema
 import datetime
 
@@ -6,6 +8,10 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         
-        one_hour_ago = datetime.datetime.now() - datetime.timedelta(minutes=60)
+        one_hour_ago = timezone.now() - datetime.timedelta(minutes=60)
         schemas = Schema.objects.filter(finished_date__lt = one_hour_ago)
+        schemas.delete()
+
+        one_day_ago = timezone.now() - datetime.timedelta(hours=24)
+        schemas = Schema.objects.filter(created_date__lt=one_day_ago)
         schemas.delete()
